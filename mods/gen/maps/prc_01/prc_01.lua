2ce6ba286e36aaa3e816758499860cc97effd427
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2018-07-27 13:54:39 +0300

    Improve PRC Mission 1
    It now pretty much works, but there is still some problems.

--- mods/gen/maps/prc_01/prc_01.lua
+++ mods/gen/maps/prc_01/prc_01.lua
@@ -9,8 +9,17 @@
 
 Cars = { car1, car2 }
 CarBunkers = { PRCBunker1, PRCBunker2 }
+CarBunkerLocations = { PRCBunker1.Location, PRCBunker2.Location }
+CarTrigger = { CPos.New(28, 32), CPos.New(29, 32), CPos.New(30, 33), CPos.New(31, 33), CPos.New(32, 33), CPos.New(33, 33), CPos.New(34, 32), CPos.New(35, 32) }
+HardCars = { car3, car4 }
+HardCarTarget = PRCSupply
+Terrorists = { SplodeGuy1, SplodeGuy2 }
+BaseAttackForce = { EasyTech1, EasyTech2, AttackInf1, AttackInf2, AttackInf3, AttackInf4 }
+BaseAttackForceLocation = PRCCommand.Location + CVec.New(1, 2)
 
 Battlemasters = { Battlemaster1, Battlemaster2, Battlemaster3, Battlemaster4, Battlemaster5 }
+BattlemasterTriggers = { Actor703, Actor705 }
+
 Survivors1 = { SurvivorBM1, SurvivorBM2, SurvivorDT1, SurvivorDT2 }
 Survivors2 = { SurvivorOT1, SurvivorOT2 }
 
@@ -25,20 +34,44 @@ StartingCash =
 	hard = 10000,
 }
 
-CarAttackTimes =
-{
-	easy = DateTime.Seconds(10),
-	normal = DateTime.Seconds(7),
-	hard = DateTime.Seconds(5)
-}
-
 BunkerCarAttack = function(number)
 	local car = Cars[number]
 
-	car.Owner = gla
+	if not car.IsDead then
+		car.Owner = gla
 
-	PlaceMeBomb(car)
-	car.DetonateAttack(CarBunkers[number])
+		PlaceMeBomb(car)
+		car.Move(CarBunkerLocations[number] + CVec.New(0, 2))
+		car.DetonateAttack(CarBunkers[number])
+	end
+end
+
+BaseAttack = function()
+	Utils.Do(HardCars, function(car)
+		if Difficulty == "hard" and not car.IsDead then
+			car.Owner = gla
+			PlaceMeBomb(car)
+			car.DetonateAttack(HardCarTarget)
+		end
+	end)
+
+	Utils.Do(BaseAttackForce, function(attacker)
+		if not attacker.IsDead then
+			attacker.AttackMove(BaseAttackForceLocation)
+		end
+	end)
+
+	Utils.Do(Terrorists, function(terrorist)
+		if not terrorist.IsDead then
+			terrorist.DetonateAttack(PRCCommand)
+
+			Trigger.OnDamaged(terrorist, function()
+				if terrorist.Health <= 1500 then
+					terrorist.Detonate()
+				end
+			end)
+		end
+	end)
 end
 
 PlaceMeBomb = function(unit)
@@ -47,7 +80,11 @@ end
 
 SendMiG = function(entry, target)
 	local mig = Reinforcements.Reinforce(prc, MiG, { entry.Location, entry.Location })
-	mig[1].Attack(target)
+	Utils.Do(mig, function(a)
+		a.Attack(target)
+		a.Move(entry.Location)
+		a.Destroy()
+	end)
 end
 
 SetupDifficulties = function()
@@ -58,9 +95,18 @@ SetupDifficulties = function()
 		GLAStinger2.Destroy()
 		MediumTech1.Destroy()
 		MediumTech2.Destroy()
+		MediumTech4.Destroy()
+		MediumTech5.Destroy()
+		MediumScorp1.Destroy()
+		MediumScorp2.Destroy()
+		MediumScorp3.Destroy()
+		MediumScorp4.Destroy()
+		MediumScorp5.Destroy()
 		AttackInf1.Destroy()
 		AttackInf2.Destroy()
 		AttackInf6.Destroy()
+		AttackInf9.Destroy()
+		AttackInf10.Destroy()
 		SplodeGuy1.Destroy()
 		SplodeGuy2.Destroy()
 	end
@@ -68,6 +114,7 @@ SetupDifficulties = function()
 		EasyTech1.Destroy()
 		EasyTech2.Destroy()
 		EasyTech3.Destroy()
+		EasyScorp1.Destroy()
 		AttackInf3.Destroy()
 		AttackInf4.Destroy()
 	end
@@ -109,43 +156,75 @@ WorldLoaded = function()
 		ReducePoints(player)
 	end
 
+	Media.PlaySpeechNotification(prc, "MIS_PRC1_A")
+
 	Difficulty = Map.LobbyOption("difficulty")
 	SetupDifficulties()
 
 	Camera.Position = PRCCommand.CenterPosition
 	InitObjectives()
-	
+
 	Trigger.OnEnteredFootprint(MiGTrigger, function(a, id)
-		Trigger.RemoveFootprintTrigger(id)
+		if a.Owner == prc then
+			Trigger.RemoveFootprintTrigger(id)
+			
+			Media.PlaySpeechNotification(prc, "MIS_PRC1_C")
 
-		SendMiG(MiGEntry1, MiGCutsceneST1)
-		SendMiG(MiGEntry2, MiGCutsceneTech2)
-		SendMiG(MiGEntry3, MiGCutsceneTech4)
-		SendMiG(MiGEntry4, MiGCutsceneInf3)
+			local units = prc.GetGroundAttackers()
+			Utils.Do(units, function(a)
+				a.Stop()
+			end)
 
-		Utils.Do(MiGTargets, function(a)
-			a.AttackMove(MiGTargetsMoveWaypoint.Location)
-		end)
+			Trigger.AfterDelay(DateTime.Seconds(6), function()
+				Media.PlaySpeechNotification(prc, "MIS_PRC1_G")
+				Camera.Position = MiGRevealWaypoint.CenterPosition
 
-		Camera.Position = MiGRevealWaypoint.CenterPosition
-		local camera = Actor.Create("camera.spyplane", true, { Owner = prc, Location = MiGRevealWaypoint.Location })
-		Trigger.AfterDelay(DateTime.Seconds(30), function()
-			camera.Destroy()
-		end)
-	end)
+				SendMiG(MiGEntry1, MiGCutsceneST1)
+				SendMiG(MiGEntry2, MiGCutsceneTech2)
+				SendMiG(MiGEntry3, MiGCutsceneTech4)
+				SendMiG(MiGEntry4, MiGCutsceneInf3)
 
-	Trigger.AfterDelay(CarAttackTimes[Difficulty], function()
-		BunkerCarAttack(1)
-		BunkerCarAttack(2)
-	end)
+				Utils.Do(MiGTargets, function(a)
+					a.AttackMove(MiGTargetsMoveWaypoint.Location)
+				end)
 
-	Trigger.OnDiscovered(BattlemasterTrigger, function(discovered, discoverer)
-		if discoverer == prc then
-			Utils.Do(Battlmeasters, function(a)
-				a.Owner = prc
-				a.Move(BattlemasterWaypoint)
+				Trigger.OnAllKilled(MiGTargets, function()
+					Media.PlaySpeechNotification(prc, "MIS_PRC1_H")
+				end)
+
+				local camera = Actor.Create("camera.spyplane", true, { Owner = prc, Location = MiGRevealWaypoint.Location })
+				Trigger.AfterDelay(DateTime.Seconds(30), function()
+					camera.Destroy()
+
+					Media.PlaySpeechNotification(prc, "MIS_PRC1_D")
+				end)
 			end)
 		end
+	end)
+
+	Trigger.OnEnteredFootprint(CarTrigger, function(a, id)
+		if a.Owner == prc then
+			Trigger.RemoveFootprintTrigger(id)
+
+			Media.PlaySpeechNotification(prc, "MIS_PRC1_I")
+			Trigger.AfterDelay(DateTime.Seconds(5), function()
+				BunkerCarAttack(1)
+				BunkerCarAttack(2)
+			end)
+
+			Trigger.AfterDelay(DateTime.Seconds(10), function()
+				BaseAttack()
+			end)
+		end
+	end)
+
+	Trigger.OnAllKilled(BattlemasterTriggers, function()
+		Utils.Do(Battlemasters, function(a)
+			a.Owner = prc
+			a.Move(BattlemasterWaypoint.Location)
+		end)
+
+		Media.PlaySpeechNotification(prc, "MIS_PRC1_B")
 	end)
 
 	Utils.Do(Survivors1, function(survivor)
@@ -154,6 +233,8 @@ WorldLoaded = function()
 				Utils.Do(Survivors1, function(a)
 					a.Owner = prc
 				end)
+
+				Media.PlaySpeechNotification(prc, "MIS_PRC1_E")
 			end
 		end)
 	end)
@@ -164,8 +245,14 @@ WorldLoaded = function()
 				Utils.Do(Survivors2, function(a)
 					a.Owner = prc
 				end)
+
+				Media.PlaySpeechNotification(prc, "MIS_PRC1_F")
 			end
 		end)
+	end)
+
+	Trigger.OnKilled(GLATunnel1, function()
+		Media.PlaySpeechNotification(prc, "MIS_PRC1_J")
 	end)
 
 	Trigger.OnKilled(GLANukeBunker, function()
