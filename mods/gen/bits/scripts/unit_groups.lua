f18191a5d49807d99a01fbefbef53a66e9f627ba
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2018-08-02 12:25:36 +0300

    Update Challange - Kwai.
    Still not playable. But i added a lot of taunts.
    
    Also trees and fixed a small mistake in the town.

---
+++ mods/gen/bits/scripts/unit_groups.lua
@@ -0,0 +1,204 @@
+--[[
+   Copyright 2007-2018 The OpenRA Developers (see AUTHORS)
+   This file is part of OpenRA, which is free software. It is made
+   available to you under the terms of the GNU General Public License
+   as published by the Free Software Foundation, either version 3 of
+   the License, or (at your option) any later version. For more
+   information, see COPYING.
+]]
+
+BaseBuilding =
+{
+	"building.usa_command_center",
+	"building.gla_command_center",
+	"building.prc_command_center",
+	"building.prc_command_center.radar",
+	"fake.command_center",
+	"building.advanced_cold_fusion_reactor",
+	"building.cold_fusion_reactor",
+	"building.cold_fusion_reactor.laser",
+	"building.cold_fusion_reactor.boss",
+	"building.nuclear_reactor",
+	"building.advanced_nuclear_reactor",
+	"building.usa_supply_center",
+	"building.gla_supply_stash",
+	"building.prc_supply_center",
+	"fake.supply_stash",
+	"building.usa_barracks",
+	"building.gla_barracks",
+	"building.prc_barracks",
+	"fake.barracks",
+	"building.usa_war_factory",
+	"building.arms_dealer",
+	"building.prc_war_factory",
+	"fake.arms_dealer",
+	"building.usa_airfield",
+	"building.prc_airfield",
+	"building.strategy_center",
+	"building.palace",
+	"building.propaganda_center",
+	"building.detention_camp",
+	"building.supply_drop_zone",
+	"building.black_market",
+	"building.internet_center",
+	"fake.black_market"
+}
+
+BaseDefense =
+{
+	"building.patriot",
+	"building.emp_patriot",
+	"building.laser_turret",
+	"building.patriot.boss",
+	"building.firebase",
+	"building.tunnel_network",
+	"building.tunnel_network.no_free_actor",
+	"building.toxin_tunnel_network",
+	"building.toxin_tunnel_network.no_free_actor",
+	"building.tunnel_network.boss",
+	"building.tunnel_network.boss.no_free_actor",
+	"building.stinger_site",
+	"building.gatling_cannon",
+	"building.bunker",
+	"building.fortified_bunker",
+	"fake.tunnel_network",
+	"fake.toxin_tunnel_network",
+	"fake.stinger_site"
+}
+
+Infantry =
+{
+	"infantry.ranger",
+	"infantry.missile_defender",
+	"infantry.pathfinder",
+	"infantry.pathfinder.boss",
+	"infantry.rebel",
+	"infantry.toxin_rebel",
+	"infantry.rpg_trooper",
+	"infantry.terrorist",
+	"infantry.toxin_terrorist",
+	"infantry.angry_mob",
+	"infantry.angry_mob.boss",
+	"infantry.hijacker",
+	"infantry.hijacker.stealth",
+	"infantry.saboteur",
+	"infantry.red_guard",
+	"infantry.minigunner",
+	"infantry.tank_hunter",
+	"infantry.hacker",
+	"infantry.super_hacker",
+	"infantry.hacker.boss",
+	"infantry.conscript",
+	"infantry.grenadier",
+	"infantry.flamethrower"
+}
+
+Tank =
+{
+	"vehicle.crusader_tank",
+	"vehicle.paladin_tank",
+	"vehicle.paladin_tank.boss",
+	"vehicle.scorpion_tank",
+	"vehicle.marauder_tank",
+	"vehicle.battlemaster_tank",
+	"vehicle.nuclear_battlemaster_tank",
+	"vehicle.overlord_tank",
+	"vehicle.emparor_overlord"
+}
+
+Plane =
+{
+	"aircraft.raptor",
+	"aircraft.king_raptor",
+	"aircraft.king_raptor.boss",
+	"aircraft.stealth_fighter",
+	"aircraft.stealth_fighter.air",
+	"aircraft.aurora",
+	"aircraft.aurora_alpha",
+	"aircraft.aurora.boss",
+	"aircraft.mig"
+}
+
+CommandCenter =
+{
+	"building.usa_command_center",
+	"building.gla_command_center",
+	"building.prc_command_center",
+	"building.prc_command_center.radar",
+	"fake.command_center"
+}
+
+Barracks =
+{
+	"building.usa_barracks",
+	"building.gla_barracks",
+	"building.prc_barracks",
+	"fake.barracks"
+}
+
+WarFactory =
+{
+	"building.usa_war_factory",
+	"building.arms_dealer",
+	"building.prc_war_factory",
+	"fake.arms_dealer"
+}
+
+Airfield =
+{
+	"building.usa_airfield",
+	"building.prc_airfield"
+}
+
+ParticleCannon =
+{
+	"building.particle_cannon",
+	"building.particle_cannon.super",
+	"building.particle_cannon.boss"
+}
+
+ScudStorm =
+{
+	"building.scud_storm",
+	"building.scud_storm.boss",
+	"fake.scud_storm"
+}
+
+MissileSilo =
+{
+	"building.missile_silo",
+	"building.missile_silo.boss"
+}
+
+Burton =
+{
+	"infantry.colonel_burton",
+	"infantry.colonel_burton.boss"
+}
+
+Jarmen =
+{
+	"infantry.jarmen_kell",
+	"infantry.jarmen_kell.demo",
+	"infantry.jarmen_kell.boss"
+}
+
+Lotus =
+{
+	"infantry.black_lotus",
+	"infantry.super_lotus",
+	"infantry.black_lotus.boss"
+}
+
+TechBuilding =
+{
+	"tech.oil_derrick",
+	"tech.oil_refinery",
+	"tech.hospital",
+	"tech.power_plant",
+	"tech.reinforcements_pad",
+	"tech.soviet_barracks",
+	"tech.shipyard",
+	"tech.repair_bay",
+	"tech.communications_center"
+}
--- mods/gen/maps/challange-tank/challange_tank.lua
+++ mods/gen/maps/challange-tank/challange_tank.lua
@@ -7,9 +7,18 @@
    information, see COPYING.
 ]]
 
-Barracks	= { "building.usa_barrakcs", "building.gla_barracks", "building.prc_barracks"}
-WarFactory	= { "building.usa_war_factory", "building.arms_dealer", "building.prc_war_factory"}
-Airfield	= { "building.usa_airfield", "building.prc_airfield"}
+RandomTaunts = { "19", "15", "16", "17", "18", "20", "22", "23", "24", "25", "26", "27", "29", "30" }
+
+ResearchUpgrade = function(building, upgrade)
+	local buildings = enemy.GetActorsByType(building)
+	if #buildings > 0 then
+		buildings[1].Build( { upgrade } )
+	else
+		Trigger.AfterDelay(DateTime.Minutes(1), function()
+			ResearchUpgrade(building, upgrade)
+		end)
+	end
+end
 
 GiveGeneralPowers = function()
 	Actor.Create("generals_power.carpet_bombing",		true, { Owner = enemy })
@@ -47,10 +56,105 @@ DifficultySetup = function()
 	end
 end
 
+lowPowerTauntTimer = 0
+randomTauntTimer = Utils.RandomInteger(DateTime.Seconds(45), DateTime.Seconds(120))
+randomTauntToPlay = 1
 Tick = function()
+	randomTauntTimer = randomTauntTimer - 1
+	if randomTauntTimer == 0 then
+		randomTauntTimer = Utils.RandomInteger(DateTime.Seconds(45), DateTime.Seconds(120))
+		Media.PlayTauntNotification(enemy, RandomTaunts[randomTauntToPlay])
+
+		if (randomTauntToPlay == 14) then
+			randomTauntToPlay = 1
+		else
+			randomTauntToPlay = randomTauntToPlay + 1
+		end
+	end
+
 	if 301 > player.Cash and not lowCashTauntPlayed then
 		lowCashTauntPlayed = true
 		Media.PlayTauntNotification(enemy, "61")
+	end
+
+	if player.PowerState == "Low" or player.PowerState == "Critical" then
+		if not lowPowerTaunt1Played then
+			lowPowerTaunt1Played = true
+			Media.PlayTauntNotification(enemy, "59")
+		end
+
+		if not lowPowerTaunt2Played and playerRecoveredFromFirstLowPower then
+			lowPowerTaunt2Played = true
+			Media.PlayTauntNotification(enemy, "60")
+		end
+	end
+
+	if lowPowerTaunt1Played then
+		lowPowerTauntTimer = lowPowerTauntTimer + 1
+		if player.PowerState == "Normal" and 1501 < lowPowerTauntTimer then
+			playerRecoveredFromFirstLowPower = true
+		end
+	end
+
+	if not barrBuildTauntPlayed and #player.GetActorsByTypes(Barracks) > 0 then
+		barrBuildTauntPlayed = true
+		Media.PlayTauntNotification(enemy, "80")
+	end
+	if not wfacBuildTauntPlayed and #player.GetActorsByTypes(WarFactory) > 0 then
+		wfacBuildTauntPlayed = true
+		Media.PlayTauntNotification(enemy, "79")
+	end
+	if not airfBuildTauntPlayed  and #player.GetActorsByTypes(Airfield) > 0 then
+		airfBuildTauntPlayed = true
+		Media.PlayTauntNotification(enemy, "78")
+	end
+	if not oildBuildTauntPlayed  and #player.GetActorsByType("tech.oil_derrick") > 0 then
+		oildBuildTauntPlayed = true
+		Media.PlayTauntNotification(enemy, "48")
+	end
+	if not pcanBuildTauntPlayed  and #player.GetActorsByTypes(ParticleCannon) > 0 then
+		pcanBuildTauntPlayed = true
+		Media.PlayTauntNotification(enemy, "65")
+	end
+	if not scudBuildTauntPlayed  and #player.GetActorsByTypes(ScudStorm) > 0 then
+		scudBuildTauntPlayed = true
+		Media.PlayTauntNotification(enemy, "63")
+	end
+	if not nukeBuildTauntPlayed  and #player.GetActorsByTypes(MissileSilo) > 0 then
+		nukeBuildTauntPlayed = true
+		Media.PlayTauntNotification(enemy, "64")
+	end
+	if not brtnBuildTauntPlayed  and #player.GetActorsByTypes(Burton) > 0 then
+		brtnBuildTauntPlayed = true
+		Media.PlayTauntNotification(enemy, "75")
+	end
+	if not jrmnBuildTauntPlayed  and #player.GetActorsByTypes(Jarmen) > 0 then
+		jrmnBuildTauntPlayed = true
+		Media.PlayTauntNotification(enemy, "77")
+	end
+	if not lotsBuildTauntPlayed  and #player.GetActorsByTypes(Lotus) > 0 then
+		lotsBuildTauntPlayed = true
+		Media.PlayTauntNotification(enemy, "76")
+	end
+	if not builBuildTauntPlayed  and #player.GetActorsByTypes(BaseBuilding) > 7 then
+		builBuildTauntPlayed = true
+		Media.PlayTauntNotification(enemy, "70")
+	end
+	if not defeBuildTauntPlayed  and #player.GetActorsByTypes(BaseDefense) > 5 then
+		defeBuildTauntPlayed = true
+		Media.PlayTauntNotification(enemy, "66")
+	end
+	if not infaBuildTauntPlayed  and #player.GetActorsByTypes(Infantry) > 11 then
+		infaBuildTauntPlayed = true
+		Media.PlayTauntNotification(enemy, "69")
+	end
+	if not tankBuildTauntPlayed  and #player.GetActorsByTypes(Tank) > 5 then
+		tankBuildTauntPlayed = true
+		Media.PlayTauntNotification(enemy, "67")
+	end
+	if not planBuildTauntPlayed  and #player.GetActorsByTypes(Plane) > 3 then
+		planBuildTauntPlayed = true
+		Media.PlayTauntNotification(enemy, "68")
 	end
 end
 
@@ -65,7 +169,25 @@ WorldLoaded = function()
 	end
 
 	Difficulty = Map.LobbyOption("difficulty")
-	
+
 	DifficultySetup()
 	GiveGeneralPowers()
+
+	ResearchUpgrade("building.prc_war_factory", "upgrade.chain_gun")
+	ResearchUpgrade("building.prc_airfield", "upgrade.mig_armor")
+	ResearchUpgrade("building.propaganda_center", "upgrade.nationalism")
+	ResearchUpgrade("building.missile_silo", "upgrade.nuclear_tanks")
+
+	Trigger.OnAnyKilled(enemy.GetActorsByTypes(CommandCenter), function()
+		Media.PlayTauntNotification(enemy, "44")
+	end)
+	Trigger.OnAnyKilled(enemy.GetActorsByTypes(Barracks), function()
+		Media.PlayTauntNotification(enemy, "39")
+	end)
+	Trigger.OnAnyKilled(enemy.GetActorsByTypes(WarFactory), function()
+		Media.PlayTauntNotification(enemy, "40")
+	end)
+	Trigger.OnAnyKilled(enemy.GetActorsByTypes(Airfield), function()
+		Media.PlayTauntNotification(enemy, "41")
+	end)
 end
