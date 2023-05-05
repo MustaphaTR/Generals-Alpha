a56f31225dbc8ec81866093b8cf1c9f8ee032dc0
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2023-05-05 22:26:52 +0300

    Fix Paradrops on shellmaps.

--- mods/gen/maps/desert-shellmap/desert-shellmap.lua
+++ mods/gen/maps/desert-shellmap/desert-shellmap.lua
@@ -1,5 +1,5 @@
 --[[
-   Copyright 2007-2018 The OpenRA Developers (see AUTHORS)
+   Copyright 2007-2020 The OpenRA Developers (see AUTHORS)
    This file is part of OpenRA, which is free software. It is made
    available to you under the terms of the GNU General Public License
    as published by the Free Software Foundation, either version 3 of
@@ -178,7 +178,7 @@ end
 
 SendParadrop = function()
 	local lz = Utils.Random(ParadropWaypoints)
-	local units = powerproxy.SendParatroopers(lz.CenterPosition)
+	local units = powerproxy.TargetParatroopers(lz.CenterPosition)
 
 	Utils.Do(units, function(a)
 		BindActorTriggers(a)
