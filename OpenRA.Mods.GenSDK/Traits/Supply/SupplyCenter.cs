a6403d646bf223951f6fde059c860f775df8b3f7
dnqbob <dnqbob@outlook.com>
2023-08-29 20:09:11 +0800

    Fix Supply traits and acitvity:
    1. avoid non-aircraft stuck, at max effort
    2. add manual control
    3. support aircraft
    4. introduce collect/deliver one by one, which is General style, make the ecomomy more general.

--- OpenRA.Mods.GenSDK/Traits/Supply/SupplyCenter.cs
+++ OpenRA.Mods.GenSDK/Traits/Supply/SupplyCenter.cs
@@ -19,7 +19,7 @@ using OpenRA.Traits;
 
 namespace OpenRA.Mods.GenSDK.Traits
 {
-	public class SupplyCenterInfo : TraitInfo
+	public class SupplyCenterInfo : TraitInfo, Requires<BuildingInfo>
 	{
 		public readonly HashSet<string> SupplyTypes = new() { "supply" };
 
@@ -33,8 +33,22 @@ namespace OpenRA.Mods.GenSDK.Traits
 		[Desc("Where can the supply collectors can place the supplies.")]
 		public readonly CVec[] DeliveryOffsets = Array.Empty<CVec>();
 
-		[Desc("Collector faces this way before dropping the supplies; if undefined, faces towards the center of the actor.")]
-		public readonly WAngle? Facing = null;
+		[Desc("Trade supplies to only one collector in dock at a time.")]
+		public readonly bool DeliverOneByOne = true;
+
+		[Desc("When a collector is comming and working for delivering (HACK: not waiting), occupancy will be inceased by collector.",
+			"Note: only when both `ToleratedOccupancy` and `ToleratedStuckRoad` are reached,",
+			"this dock will prevent non-aircraft auto-select this dock, and suggest aircraft not to auto-select this dock")]
+		public readonly int ToleratedOccupancy = 6;
+
+		[Desc("When a collector is within `StuckRoadCheckRange`, road stucking will be inceased by collector")]
+		public readonly int ToleratedStuckRoad = 9;
+
+		[Desc("Road stucking checking range, and force make way range when entry is stuck.")]
+		public readonly WDist StuckRoadCheckRange = WDist.FromCells(3);
+
+		[Desc("Force all collectors with `StuckRoadCheckRange` to make way after this delay, if a collector stuck on `DeliveryOffsets`.")]
+		public readonly int ForceMakeWayDelay = 18;
 
 		public readonly bool ShowTicks = true;
 		public readonly int TickLifetime = 30;
@@ -44,14 +58,20 @@ namespace OpenRA.Mods.GenSDK.Traits
 		public override object Create(ActorInitializer init) { return new SupplyCenter(init.Self, this); }
 	}
 
-	public class SupplyCenter : ITick, IResourceExchange, INotifyCreated, INotifyOwnerChanged
+	public sealed class SupplyCenter : ITick, IResourceExchange, INotifyCreated, INotifyOwnerChanged
 	{
+		const int CheckDelivererOnDockTick = 61;
+		int checkTick;
+
 		public readonly SupplyCenterInfo Info;
 		PlayerResources playerResources;
 		IResourceValueModifier[] resourceMultipliers;
 
+		public readonly Dictionary<CPos, HashSet<CPos>> NoParkingOffsets = new();
+
 		int currentDisplayTick = 0;
 		int currentDisplayValue = 0;
+		Actor collectorInTrade;
 
 		public SupplyCenter(Actor self, SupplyCenterInfo info)
 		{
@@ -63,6 +83,25 @@ namespace OpenRA.Mods.GenSDK.Traits
 		void INotifyCreated.Created(Actor self)
 		{
 			resourceMultipliers = self.TraitsImplementing<IResourceValueModifier>().ToArray();
+			checkTick = self.World.SharedRandom.Next(0, CheckDelivererOnDockTick);
+
+			var bi = self.Info.TraitInfoOrDefault<BuildingInfo>();
+			if (bi == null)
+				return;
+
+			foreach (var offset in Info.DeliveryOffsets)
+			{
+				var entrys = new List<CPos>();
+				foreach (var direction in CVec.Directions)
+				{
+					entrys.AddRange(new List<CVec>() { offset }.Select(c => c + direction).SkipWhile(
+						e => Info.DeliveryOffsets.Contains(e) ||
+						(bi.Footprint.TryGetValue(e, out var r) && r is not FootprintCellType.OccupiedPassable or FootprintCellType.Empty)).
+						Select(c => c + self.Location));
+				}
+
+				NoParkingOffsets.Add(offset + self.Location, entrys.ToHashSet());
+			}
 		}
 
 		public bool CanGiveResource(int amount) { return !Info.UseStorage || Info.DiscardExcessResources || playerResources.CanGiveResources(amount); }
@@ -85,6 +124,84 @@ namespace OpenRA.Mods.GenSDK.Traits
 				currentDisplayValue += amount;
 		}
 
+		public bool RegisterTrade(Actor self, Actor collector)
+		{
+			if (!Info.DeliverOneByOne || collector == collectorInTrade)
+				return true;
+
+			if (collectorInTrade == null || collectorInTrade.IsDead || !collectorInTrade.IsInWorld)
+			{
+				collectorInTrade = collector;
+				return true;
+			}
+
+			return false;
+		}
+
+		public bool AtDeliveryZone(Actor self, CPos location)
+		{
+			if (NoParkingOffsets.ContainsKey(location))
+				return true;
+
+			return false;
+		}
+
+		public bool AtNoParkingZone(Actor self, CPos location)
+		{
+			if (NoParkingOffsets.ContainsKey(location) || NoParkingOffsets.Values.Any(e => e.Contains(location)))
+				return true;
+
+			return false;
+		}
+
+		public void UnregisterTrade(Actor self, Actor collector)
+		{
+			if (!Info.DeliverOneByOne)
+				return;
+
+			if (collector == collectorInTrade)
+				collectorInTrade = null;
+		}
+
+		public void ShouldChangeCenter(Actor self, Actor collector)
+		{
+			var collectorTrait = collector.TraitOrDefault<SupplyCollector>();
+			if (collectorTrait == null)
+				return;
+
+			var stuckroad = 0;
+			var occupancy = 0;
+			foreach (var c in self.World.ActorsWithTrait<SupplyCollector>())
+			{
+				if (CanStuckRoad(self, c.Actor))
+					stuckroad += collectorTrait.IsAircraft ? c.Trait.Info.StuckRoadPowerAir : c.Trait.Info.StuckRoadPower;
+
+				if (c.Trait.DeliveryBuilding == self && SupplyCollector.IsCollectorBusyOnDelivering(c))
+					occupancy += c.Trait.Info.OccupancyPower;
+			}
+
+			if (stuckroad > Info.ToleratedStuckRoad && occupancy > Info.ToleratedOccupancy)
+				collectorTrait.FindOtherDeliveryBuildingAdvisor = self;
+		}
+
+		public bool CanStuckRoad(Actor self, Actor collector)
+		{
+			if ((self.CenterPosition - collector.CenterPosition).HorizontalLengthSquared <= Info.StuckRoadCheckRange.LengthSquared)
+				return true;
+			return false;
+		}
+
+		public void HandlingStuck(Actor self, Actor stuckCollector)
+		{
+			foreach (var c in self.World.ActorsWithTrait<SupplyCollector>().Where(c =>
+			c.Trait.Info.DeliveryRelationships.HasRelationship(c.Actor.Owner.RelationshipWith(self.Owner)) &&
+			c.Trait.WorkingAtPortState == WorkingAtPortState.None &&
+			(self.CenterPosition - c.Actor.CenterPosition).HorizontalLengthSquared <= Info.StuckRoadCheckRange.LengthSquared))
+			{
+				c.Trait.MakeWay();
+			}
+		}
+
 		void ITick.Tick(Actor self)
 		{
 			if (Info.ShowTicks && currentDisplayValue > 0 && --currentDisplayTick <= 0)
@@ -94,6 +211,28 @@ namespace OpenRA.Mods.GenSDK.Traits
 					self.World.AddFrameEndTask(w => w.Add(new FloatingText(self.CenterPosition, self.Owner.Color, FloatingText.FormatCashTick(temp), 30)));
 				currentDisplayTick = Info.TickRate;
 				currentDisplayValue = 0;
+			}
+
+			// Check if any collector blocked here but has goods to delivery
+			if (--checkTick <= 0)
+			{
+				checkTick = CheckDelivererOnDockTick;
+				foreach (var loc in NoParkingOffsets.Keys)
+				{
+					var collectors = self.World.ActorMap.GetActorsAt(loc).ToList();
+					foreach (var c in collectors)
+					{
+						var colletorTrait = (c == null || c.IsDead || !c.IsInWorld) ? null : c.Trait<SupplyCollector>();
+						if (colletorTrait != null && !colletorTrait.IsEmpty &&
+							colletorTrait.WorkingAtPortState == WorkingAtPortState.None &&
+							colletorTrait.Info.DeliveryRelationships.HasRelationship(c.Owner.RelationshipWith(self.Owner)))
+						{
+							c.CancelActivity();
+							colletorTrait.DeliveryBuilding = self;
+							colletorTrait.TakeTheChanceToDeliver();
+						}
+					}
+				}
 			}
 		}
 
--- OpenRA.Mods.GenSDK/Traits/Supply/SupplyDock.cs
+++ OpenRA.Mods.GenSDK/Traits/Supply/SupplyDock.cs
@@ -30,12 +30,26 @@ namespace OpenRA.Mods.GenSDK.Traits
 		[Desc("Where can the aircraft supply collectors can collect the supplies from.")]
 		public readonly CVec[] AircraftCollectionOffsets = Array.Empty<CVec>();
 
-		[Desc("Collector faces this way before taking the supplies; if undefined, faces towards the center of the actor.")]
-		public readonly WAngle? Facing = null;
-
 		[Desc("Conditions to grant when dock has more than specified amount of supplies.",
 			"A dictionary of [integer]: [condition].")]
 		public readonly Dictionary<int, string> FullnessConditions = new();
+
+		[Desc("Supplies check all collector reserved at this delay to pickout the invalid one.")]
+		public readonly int CheckReservedInterval = 5;
+
+		[Desc("Trade supplies to only one collector in dock at a time.")]
+		public readonly bool CollectOneByOne = true;
+
+		[Desc("When a collector is comming and working for collecting (HACK: not waiting), occupancy will be inceased by collector.",
+			"Note: only when both `ToleratedOccupancy` and `ToleratedStuckRoad` are reached,",
+			"this dock will prevent non-aircraft auto-select this dock, and suggest aircraft not to auto-select this dock")]
+		public readonly int ToleratedOccupancy = 6;
+
+		[Desc("When a collector is within `StuckRoadCheckRange`, road stucking will be inceased by collector")]
+		public readonly int ToleratedStuckRoad = 9;
+
+		[Desc("Road stucking checking range, and force make way range when entry is stuck.")]
+		public readonly WDist StuckRoadCheckRange = WDist.FromCells(3);
 
 		[GrantedConditionReference]
 		public IEnumerable<string> LinterFullnessConditions { get { return FullnessConditions.Values; } }
@@ -43,22 +57,33 @@ namespace OpenRA.Mods.GenSDK.Traits
 		public override object Create(ActorInitializer init) { return new SupplyDock(init.Self, this); }
 	}
 
-	public class SupplyDock : IProvideTooltipInfo, INotifyCreated, ISupplyDock
+	public sealed class SupplyDock : IProvideTooltipInfo, INotifyCreated, ISupplyDock, ITick
 	{
 		public readonly SupplyDockInfo Info;
 		public int Amount;
 
 		readonly Dictionary<int, int> fullnessTokens = new();
+		readonly Func<CPos> getDockLocation;
+		readonly bool[] airOffsetsTaken;
+
+		// Aircraft use a reserve and unreserve logic to make them dock in different place
+		readonly Dictionary<Actor, int> reservedAirs = new();
+		Actor collectorInTrade;
+
+		int ticks;
 
 		public SupplyDock(Actor self, SupplyDockInfo info)
 		{
 			Info = info;
 			Amount = Info.Capacity;
+			airOffsetsTaken = new bool[info.AircraftCollectionOffsets.Length];
+			getDockLocation = () => self.Location;
 		}
 
 		void INotifyCreated.Created(Actor self)
 		{
 			CheckConditions(self);
+			ticks = self.World.SharedRandom.Next(0, Info.CheckReservedInterval);
 		}
 
 		public bool IsFull { get { return Amount == Info.Capacity; } }
@@ -68,6 +93,107 @@ namespace OpenRA.Mods.GenSDK.Traits
 		bool ISupplyDock.IsFull() { return IsFull; }
 		bool ISupplyDock.IsEmpty() { return IsEmpty; }
 		int ISupplyDock.Fullness() { return Fullness; }
+
+		public void Tick(Actor self)
+		{
+			if (--ticks > 0)
+				return;
+
+			foreach (var a in reservedAirs.Keys)
+			{
+				if (a.IsDead || !a.IsInWorld)
+				{
+					if (reservedAirs.Remove(a, out var i))
+						airOffsetsTaken[i] = false;
+				}
+			}
+		}
+
+		public CPos? ReservedByAircraft(Actor collector)
+		{
+			if (reservedAirs.ContainsKey(collector))
+				return Info.AircraftCollectionOffsets[reservedAirs[collector]] + getDockLocation();
+
+			if (reservedAirs.Count >= airOffsetsTaken.Length)
+				return null;
+
+			var baseCellDist = collector.Location - getDockLocation();
+			var minDist = int.MaxValue;
+			var bestIndex = 0;
+			for (var i = 0; i < airOffsetsTaken.Length; i++)
+			{
+				if (airOffsetsTaken[i])
+					continue;
+
+				var dist = (baseCellDist - Info.AircraftCollectionOffsets[i]).LengthSquared;
+				if (dist < minDist)
+				{
+					minDist = dist;
+					bestIndex = i;
+				}
+			}
+
+			reservedAirs[collector] = bestIndex;
+			airOffsetsTaken[bestIndex] = true;
+			return Info.AircraftCollectionOffsets[reservedAirs[collector]] + getDockLocation();
+		}
+
+		public void UnreservedByAircraft(Actor collector)
+		{
+			if (reservedAirs.Remove(collector, out var i))
+				airOffsetsTaken[i] = false;
+		}
+
+		public bool RegisterTrade(Actor self, Actor collector)
+		{
+			if (!Info.CollectOneByOne || collector == collectorInTrade)
+				return true;
+
+			if (collectorInTrade == null || collectorInTrade.IsDead || !collectorInTrade.IsInWorld)
+			{
+				collectorInTrade = collector;
+				return true;
+			}
+
+			return false;
+		}
+
+		public void UnregisterTrade(Actor self, Actor collector)
+		{
+			if (!Info.CollectOneByOne)
+				return;
+
+			if (collector == collectorInTrade)
+				collectorInTrade = null;
+		}
+
+		public void ShouldChangeDock(Actor self, Actor collector)
+		{
+			var collectorTrait = collector.TraitOrDefault<SupplyCollector>();
+			if (collectorTrait == null)
+				return;
+
+			var stuckroad = 0;
+			var occupancy = 0;
+			foreach (var c in self.World.ActorsWithTrait<SupplyCollector>())
+			{
+				if (CanStuckRoad(self, c.Actor))
+					stuckroad += collectorTrait.IsAircraft ? c.Trait.Info.StuckRoadPowerAir : c.Trait.Info.StuckRoadPower;
+
+				if (c.Trait.CollectionBuilding == self && SupplyCollector.IsCollectorBusyOnCollecting(c))
+					occupancy += c.Trait.Info.OccupancyPower;
+			}
+
+			if (stuckroad > Info.ToleratedStuckRoad && occupancy > Info.ToleratedOccupancy)
+				collectorTrait.FindOtherCollectionBuildingAdvisor = self;
+		}
+
+		public bool CanStuckRoad(Actor self, Actor collector)
+		{
+			if ((self.CenterPosition - collector.CenterPosition).HorizontalLengthSquared <= Info.StuckRoadCheckRange.LengthSquared)
+				return true;
+			return false;
+		}
 
 		public void CheckConditions(Actor self)
 		{
